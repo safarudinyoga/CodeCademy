4620fe66c9eb360fdd18926952892a95fd9eeef0
safarudinyoga <safarudinyoga@gmail.com>
2019-10-16 16:40:12 +0700

    Learn Loop

---
+++ Loops/looparray.js
@@ -0,0 +1,6 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+
+for (let i=0; i < vacationSpots.length; i++){
+  console.log(`I would love to visit ${vacationSpots[i]}`)
+}
+// Write your code below
